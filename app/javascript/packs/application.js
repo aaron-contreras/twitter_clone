5ebce0983d913860ec516507e31b2f20d7883f5c
Aaron Contreras <aaronlcaq@gmail.com>
2021-10-25 16:16:33 -0500

    Import matestack's javascript and bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,22 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
+
+// Matestack
+import Vue from 'vue/dist/vue.esm'
+import Vuex from 'vuex'
+import MatestackUiCore from 'matestack-ui-core'
+let matestackUiApp = undefined
+document.addEventListener('DOMContentLoaded', () => {
+  matestackUiApp = new Vue({
+    el: "#matestack-ui",
+    store: MatestackUiCore.store
+  })
+})
