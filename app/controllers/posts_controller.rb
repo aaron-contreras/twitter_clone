16702b40135cdf995c03720a08b3cc33781fc45c
Aaron Contreras <aaronlcaq@gmail.com>
2021-10-27 12:11:40 -0500

    Modify create action to work with reactive matestack_form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,14 +13,15 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+      render json: {
+        message: 'Post was successfully created.'
+      }, status: :created
+    else
+      render json: {
+        errors: @post.errors,
+        message: 'Post could not be created.'
+      }, status: :unprocessable_entity
     end
   end
 
